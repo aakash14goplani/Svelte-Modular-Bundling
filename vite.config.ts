8fcb75ce878a9095fe8f327abfd384228dd2cf11
Aakash <goplaniaakash14@gmail.com>
2022-10-26 18:54:35 +0530

    updated commands in package file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Svelte-Modular-Bundling/',
   build: {
     sourcemap: true,
     chunkSizeWarningLimit: 2500
